069dae747e52e499c6232f77f3df9813828a3a01
Anna Offenwanger <dev@offenwanger.ca>
2022-08-29 17:27:08 +0200

    simplified the example

--- force-directed-graph/experiment1.js
+++ force-directed-graph/experiment1.js
@@ -1,58 +1,91 @@
-// Copyright 2021 Observable, Inc.
-// Released under the ISC license.
-// https://observablehq.com/@d3/disjoint-force-directed-graph
-function ForceGraph({
-    nodes, // an iterable of node objects (typically [{id}, …])
-    links // an iterable of link objects (typically [{source, target}, …])
-}, {
-    nodeId = d => d.id, // given d in nodes, returns a unique identifier (string)
-    nodeGroup, // given d in nodes, returns an (ordinal) value for color
-    nodeGroups, // an array of ordinal values representing the node groups
-    nodeTitle, // given d in nodes, a title string
-    nodeFill = "currentColor", // node stroke fill (if not using a group color encoding)
-    nodeStroke = "#fff", // node stroke color
-    nodeStrokeWidth = 1.5, // node stroke width, in pixels
-    nodeStrokeOpacity = 1, // node stroke opacity
-    nodeRadius = 5, // node radius, in pixels
-    nodeStrength,
-    linkSource = ({ source }) => source, // given d in links, returns a node identifier string
-    linkTarget = ({ target }) => target, // given d in links, returns a node identifier string
-    linkStroke = "#999", // link stroke color
-    linkStrokeOpacity = 0.6, // link stroke opacity
-    linkStrokeWidth = 1.5, // given d in links, returns a stroke width in pixels
-    linkStrokeLinecap = "round", // link stroke linecap
-    linkStrength,
-    colors = d3.schemeTableau10, // an array of color strings, for the node groups
-    width = 640, // outer width, in pixels
-    height = 400, // outer height, in pixels
-    invalidation // when this promise resolves, stop the simulation
-} = {}) {
-    let container = d3.select('#svg_container');
-    
-    // Compute values.
-    const N = d3.map(nodes, nodeId).map(intern);
-    const LS = d3.map(links, linkSource).map(intern);
-    const LT = d3.map(links, linkTarget).map(intern);
-    if (nodeTitle === undefined) nodeTitle = (_, i) => N[i];
-    const T = nodeTitle == null ? null : d3.map(nodes, nodeTitle);
-    const G = nodeGroup == null ? null : d3.map(nodes, nodeGroup).map(intern);
-    const W = typeof linkStrokeWidth !== "function" ? null : d3.map(links, linkStrokeWidth);
+function createGraph() {
+    // Our data we're working with
+    let line = [
+        { x: 100, y: 150 },
+        { x: 110, y: 140 },
+        { x: 120, y: 120 },
+        { x: 150, y: 80 },
+        { x: 90, y: 70 },
+        { x: 40, y: 60 },
+        { x: 10, y: 50 }
+    ]
+
+    let linePairs = line.reduce(function (result, value, index, array) {
+        if (index < line.length - 1)
+            result.push([value, array[index + 1]]);
+        return result;
+    }, []);
+
+    // Draw it. 
+    let width = 1000; // outer width; in pixels
+    let height = 680; // outer height; in pixels
+    const svg = d3.select('#svg_container').append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-width / 2, -height / 2, width, height])
+        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+
+    svg.append("g")
+        .attr("transform", "translate(-200,0)")
+        .attr("stroke", "black")
+        .attr("stroke-width", 2)
+        .selectAll("line")
+        .data(linePairs)
+        .join("line")
+        .attr("x1", d => d[0].x)
+        .attr("y1", d => d[0].y)
+        .attr("x2", d => d[1].x)
+        .attr("y2", d => d[1].y);
+
+    svg.append("g")
+        .attr("transform", "translate(-200,0)")
+        .attr("fill", "blue")
+        .attr("stroke", "black")
+        .attr("stroke-width", 1)
+        .selectAll("circle")
+        .data(line)
+        .join("circle")
+        .attr("r", 3)
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
 
     // Replace the input nodes and links with mutable objects for the simulation.
-    nodes = d3.map(nodes, (val, i) => ({ id: N[i], fx: val.fx, fy: val.fy }));
-    links = d3.map(links, (_, i) => ({ source: LS[i], target: LT[i] }));
+    let nodes = line.map((p, index) => {
+        return {
+            x: p.x,
+            y: p.y,
+            id: index,
+        }
+    });
 
-    // Compute default domains.
-    if (G && nodeGroups === undefined) nodeGroups = d3.sort(G);
+    nodes = nodes.concat(line.map((p, index) => {
+        return {
+            fx: p.x,
+            fy: p.y,
+            id: index + line.length,
+            isFixed: true,
+        }
+    }))
 
-    // Construct the scales.
-    const color = nodeGroup == null ? null : d3.scaleOrdinal(nodeGroups, colors);
+    let links = line.map((item, index) => {
+        return {
+            source: index,
+            target: index + 1,
+        };
+    });
+    links.pop();
+    links.push(...line.map((item, index) => {
+        return {
+            source: index,
+            target: index + line.length,
+        };
+    }));
 
-    // Construct the forces.
-    const forceNode = d3.forceManyBody();
-    const forceLink = d3.forceLink(links).id(({ index: i }) => N[i]);
-    if (nodeStrength !== undefined) forceNode.strength(nodeStrength);
-    if (linkStrength !== undefined) forceLink.strength(linkStrength);
+    // default node repulsion
+    const forceNode = d3.forceManyBody().strength(-30);
+
+    const forceLink = d3.forceLink(links).id(({ index: i }) => nodes[i].id);
+    forceLink.strength(1);
 
     const simulation = d3.forceSimulation(nodes)
         .force("link", forceLink)
@@ -61,43 +94,22 @@ function ForceGraph({
         .force("y", d3.forceY())
         .on("tick", ticked);
 
-    const svg = container.append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [-width / 2, -height / 2, width, height])
-        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-
     const link = svg.append("g")
-        .attr("stroke", linkStroke)
-        .attr("stroke-opacity", linkStrokeOpacity)
-        .attr("stroke-width", typeof linkStrokeWidth !== "function" ? linkStrokeWidth : null)
-        .attr("stroke-linecap", linkStrokeLinecap)
         .selectAll("line")
         .data(links)
-        .join("line");
-
-    if (W) link.attr("stroke-width", ({ index: i }) => W[i]);
+        .join("line")
+        .attr("stroke", "#999")
+        .attr("stroke-width", 1.5);
 
     const node = svg.append("g")
-        .attr("fill", nodeFill)
-        .attr("stroke", nodeStroke)
-        .attr("stroke-opacity", nodeStrokeOpacity)
-        .attr("stroke-width", nodeStrokeWidth)
         .selectAll("circle")
         .data(nodes)
         .join("circle")
-        .attr("r", nodeRadius)
-        .call(drag(simulation));
-
-    if (G) node.attr("fill", ({ index: i }) => color(G[i]));
-    if (T) node.append("title").text(({ index: i }) => T[i]);
-
-    // Handle invalidation.
-    if (invalidation != null) invalidation.then(() => simulation.stop());
-
-    function intern(value) {
-        return value !== null && typeof value === "object" ? value.valueOf() : value;
-    }
+        .attr("fill", "green")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+        .attr("r", 5)
+        .call(getD3Drag(simulation));
 
     function ticked() {
         link
@@ -111,7 +123,7 @@ function ForceGraph({
             .attr("cy", d => d.y);
     }
 
-    function drag(simulation) {
+    function getD3Drag(simulation) {
         function dragstarted(event) {
             if (!event.active) simulation.alphaTarget(0.3).restart();
             event.subject.fx = event.subject.x;
@@ -134,62 +146,8 @@ function ForceGraph({
             .on("drag", dragged)
             .on("end", dragended);
     }
-
-    return Object.assign(svg.node(), { scales: { color } });
 }
 
-let line = [
-    { x: 100, y: 150 },
-    { x: 110, y: 140 },
-    { x: 120, y: 120 },
-    { x: 150, y: 80 },
-    { x: 90, y: 70 },
-    { x: 40, y: 60 },
-    { x: 10, y: 50 }
-]
-
-let nodes = line.map((item, index) => {
-    return {
-        id: index
-    };
-});
-
-nodes.push(...line.map((item, index) => {
-    return {
-        id: index + line.length,
-        fx: item.x,
-        fy: item.y,
-    };
-}));
-
-console.log(nodes)
-
-let links = line.map((item, index) => {
-    return {
-        source: index,
-        target: index + 1,
-        value: 1
-    };
-});
-links.pop();
-links.push(...line.map((item, index) => {
-    return {
-        source: index,
-        target: index + line.length,
-        value: 4
-    };
-}));
-
-let graph = { nodes, links }
-console.log(graph)
-
 document.addEventListener('DOMContentLoaded', function (e) {
-    chart = ForceGraph(graph, {
-        nodeId: d => d.id,
-        nodeGroup: d => d.group,
-        nodeTitle: d => `${d.id} (${d.group})`,
-        width: 1000,
-        height: 680,
-        nodeStrength:0,
-    })
+    createGraph();
 });
